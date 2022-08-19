f84454691fc5c71f0628d1a78e58fcca3769510c
Zach <zachwinger.alt@gmail.com>
2022-08-18 22:00:57 -0400

    Create CSS file and update design

--- script.js
+++ script.js
@@ -15,67 +15,50 @@ function getComputerChoice(){
     return choiceInString;
 }
 
-const roundResult = document.createElement('div');
-roundResult.classList.add('roundResult');
-container.appendChild(roundResult);
-
-
-
 function playSingleRound(playerSelection){
     let computerSelection = getComputerChoice();
-    //let playerSelectionRaw = prompt("Enter your choice");
-    //let playerSelection = playerSelectionRaw.toLowerCase();
 
     if (computerSelection == "rock"){
         if (playerSelection == "paper"){
             playerWins = playerWins + 1;
             roundResult.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
             score.textContent = `Player: ${playerWins} Computer: ${computerWins} Ties: ${tieGames}`
-            //return `You win! ${playerSelection} beats ${computerSelection}.`
         } else if (playerSelection == "scissors"){
             computerWins = computerWins + 1;
             roundResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`
             score.textContent = `Player: ${playerWins} Computer: ${computerWins} Ties: ${tieGames}`
-            //return `You lose! ${computerSelection} beats ${playerSelection}.`
         } else {
             tieGames = tieGames + 1;
             roundResult.textContent = `You tied!`;
             score.textContent = `Player: ${playerWins} Computer: ${computerWins} Ties: ${tieGames}`
-            //return `You tied!`
         }
     } else if (computerSelection == "paper"){
         if (playerSelection == "scissors"){
             playerWins = playerWins + 1;
             roundResult.textContent = `You win! ${playerSelection} beats ${computerSelection}.`
             score.textContent = `Player: ${playerWins} Computer: ${computerWins} Ties: ${tieGames}`
-            //return `You win! ${playerSelection} beats ${computerSelection}.`
         } else if (playerSelection == "rock"){
             computerWins = computerWins + 1;
             roundResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`
             score.textContent = `Player: ${playerWins} Computer: ${computerWins} Ties: ${tieGames}`
-            //return `You lose! ${computerSelection} beats ${playerSelection}.`
         } else {
             tieGames = tieGames + 1;
             roundResult.textContent = `You tied!`
             score.textContent = `Player: ${playerWins} Computer: ${computerWins} Ties: ${tieGames}`
-            //return `You tied!`
         }
     } else {
         if (playerSelection == "rock"){
             playerWins = playerWins + 1;
             roundResult.textContent = `You win! ${playerSelection} beats ${computerSelection}.`
             score.textContent = `Player: ${playerWins} Computer: ${computerWins} Ties: ${tieGames}`
-            //return `You win! ${playerSelection} beats ${computerSelection}.`
         } else if (playerSelection == "paper"){
             computerWins = computerWins + 1;
             roundResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`
             score.textContent = `Player: ${playerWins} Computer: ${computerWins} Ties: ${tieGames}`
-            //return `You lose! ${computerSelection} beats ${playerSelection}.`
         } else {
             tieGames = tieGames + 1;
             roundResult.textContent = `You tied!`
             score.textContent = `Player: ${playerWins} Computer: ${computerWins} Ties: ${tieGames}`
-            //return `You tied!`
         }
     }
 
@@ -89,27 +72,17 @@ function playSingleRound(playerSelection){
     }
 }
 
+const roundResult = document.createElement('div');
+roundResult.classList.add('resultText');
+container.appendChild(roundResult);
+
 const score = document.createElement('div');
-score.classList.add('score');
+score.classList.add('resultText');
 container.appendChild(score);
 
 const winner = document.createElement('div');
-winner.classList.add('winner');
+winner.classList.add('resultText');
 container.appendChild(winner)
-
-function game(){
-    //for (let i = 0; i < 5; i++){
-        //console.log(playSingleRound());
-    //}
-    console.log(`Final Stats: You won ${playerWins} games. You lost ${computerWins} games. You tied ${tieGames} games.`)
-    if (playerWins > computerWins){
-        console.log("Overall Winner: You");
-    } else if (computerWins > playerWins){
-        console.log("Overall Winner: Computer");
-    } else {
-        console.log("No overall winner.")
-    }
-}
 
 const buttons = document.querySelectorAll('button');
 
